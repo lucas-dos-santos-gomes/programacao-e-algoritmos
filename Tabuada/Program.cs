f818cbfeddabf18be88eab3706d37eddd373cf5d
Lucas dos Santos Gomes <lucas.sgomes26@senacsp.edu.br>
2023-04-29 00:33:24 -0300

    Finalizando exercício Tabuada

--- Tabuada/Program.cs
+++ Tabuada/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+					
+public class Tabuada
+{
+	public static void Main()
+	{
+		Console.WriteLine("Digite um número para saber a tabuada: ");
+		int number = int.Parse(Console.ReadLine());
+		Console.WriteLine("\n");
+		for(int i = 0; i <= 10; i++) {
+			int multiplication = number * i;
+			Console.WriteLine(number + " x " + i + " = " + multiplication);
+		}
+	}
+}
