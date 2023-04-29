29067cbd3f18723bd0e8f3cdacd73725874f7655
Lucas dos Santos Gomes <lucas.sgomes26@senacsp.edu.br>
2023-04-29 00:42:03 -0300

    Finalizando o projeto SomarValores

--- SomarValores/Program.cs
+++ SomarValores/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+					
+public class SomarValores
+{
+	public static void Main()
+	{
+		Console.WriteLine("Digite um número maior que 0: ");
+		int number = int.Parse(Console.ReadLine());
+		int accumulate = 0;
+		string print = ("\n" + number + "! = ");
+		for(int i = 1; i <= number; i++) {
+			accumulate += i;
+			if(i != number) {
+				print += (i + " + ");
+			} else {
+				print += (i + " = " + accumulate);
+			}
+		}
+		Console.WriteLine(print);
+	}
+}
