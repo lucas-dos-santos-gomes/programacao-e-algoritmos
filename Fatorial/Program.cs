8b309fd0e7c71b82582d95411d1204f015ce5133
Lucas dos Santos Gomes <lucas.sgomes26@senacsp.edu.br>
2023-04-29 00:47:09 -0300

    Finalizando projeto Fatorial

--- Fatorial/Program.cs
+++ Fatorial/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+					
+public class Fatorial
+{
+	public static void Main()
+	{
+		Console.WriteLine("Digite um número para fazer a contagem fatorial: ");
+		int number = int.Parse(Console.ReadLine());
+		int accumulate = 1;
+		string print = ("\n" + number + "! = ");
+		for(int i = 1; i <= number; i++) {
+			accumulate *= i;
+			if(i != number) {
+				print += (i + " x ");
+			} else {
+				print += (i + " = " + accumulate);
+			}
+		}
+		Console.WriteLine(print);
+	}
+}
