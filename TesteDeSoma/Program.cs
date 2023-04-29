2c3a220fc4e12b5752a1f4558aefd5f1ad564108
Lucas dos Santos Gomes <lucas.sgomes26@senacsp.edu.br>
2023-04-29 00:51:38 -0300

    Finalizando projeto TesteDeSoma

--- TesteDeSoma/Program.cs
+++ TesteDeSoma/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+					
+public class TesteDeSoma
+{
+	public static void Main()
+	{
+		Random gerador = new Random();
+		int n1 = gerador.Next(1, 10);
+		int n2 = gerador.Next(1, 10);
+		int resultado = n1 + n2;
+		
+		Console.WriteLine("Qual o resultado da soma de " + n1 + " + " + n2 + "?");
+		Console.WriteLine("\nPara sair digite 0");
+		int resposta = int.Parse(Console.ReadLine());
+		int erros = 0;
+		while(resposta != resultado && resposta != 0) {
+			erros++;
+			Console.WriteLine("\nTente novamente: " + n1 + " + " + n2 + " ?");
+			resposta = int.Parse(Console.ReadLine());
+		}
+
+		int porcentagemErros = 100 / (erros + 1);
+		if(resposta == 0) {
+			Console.WriteLine("\nVocê desistiu da conta");
+		} else {
+			Console.WriteLine("\nParabéns! Você acertou!");
+			Console.WriteLine(n1 + " + " + n2 +  " = " + resultado);
+			Console.WriteLine("Percentual de acertos: " + porcentagemErros + "%");
+		}
+	}
+}
