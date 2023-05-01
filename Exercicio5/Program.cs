449233f282d2f38444a9f330b0c2895ae2d6604a
Lucas dos Santos Gomes <lucas.sgomes26@senacsp.edu.br>
2023-05-01 20:12:12 -0300

    Finalizando exercício 5

--- Exercicio5/Program.cs
+++ Exercicio5/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Exercicio5
+{
+  public static void Main()
+  {
+    Console.WriteLine("Informe o primeiro número:");
+		int n1 = int.Parse(Console.ReadLine());
+		while(n1 == 0) {
+			Console.WriteLine("\nErrado! Informe um número diferente de '0':");
+			n1 = int.Parse(Console.ReadLine());
+		}
+		
+		Console.WriteLine("\nInforme o segundo número:");
+		int n2 = int.Parse(Console.ReadLine());
+		while(n2 == 0) {
+			Console.WriteLine("\nErrado! Informe um número diferente de '0':");
+			n2 = int.Parse(Console.ReadLine());
+		}
+		
+		int produto = n1 * n2;
+		Console.WriteLine("\nO produto entre '" + n1 + "' e '" + n2 + "' é = " + produto);
+  }
+}
