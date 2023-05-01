11c8d67271acd0fa46a08caa0e1f61163818154d
Lucas dos Santos Gomes <lucas.sgomes26@senacsp.edu.br>
2023-05-01 20:15:21 -0300

    Finalizando exercício 6

--- Exercicio6/Program.cs
+++ Exercicio6/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Exercicio6
+{
+  public static void Main()
+  {
+    Console.WriteLine("Informe um número qualquer:");
+		float number = float.Parse(Console.ReadLine());
+		
+		if(number < 100) {
+			Console.WriteLine("\nO número informado é menor do que 100!");
+		} else if(number > 100) {
+			Console.WriteLine("\nO número informado é maior do que 100!");
+		} else {
+			Console.WriteLine("\nO número informado é igual a 100!");
+		}
+  }
+}
