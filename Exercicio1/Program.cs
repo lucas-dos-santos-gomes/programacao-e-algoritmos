555a9c81fd70165fbfdaf09086377b11276a3867
Lucas dos Santos Gomes <lucas.sgomes26@senacsp.edu.br>
2023-05-01 19:20:55 -0300

    Finalizando exercício 1

---
+++ Exercicio1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Exercicio1
+{
+  public static void Main()
+  {
+    Console.WriteLine("Informe o primeiro número: ");
+		int n1 = int.Parse(Console.ReadLine());
+		Console.WriteLine("\nInforme o segundo número: ");
+		int n2 = int.Parse(Console.ReadLine());
+		
+		if(n1 > n2) {
+			Console.WriteLine("\nO maior número entre '" + n1 + "' e '" + n2 + "' é o '" + n1 + "'");
+		} else if(n2 > n1) {
+			Console.WriteLine("\nO maior número entre '" + n1 + "' e '" + n2 + "' é o '" + n2 + "'");
+		} else {
+			Console.WriteLine("\nOs números são iguais");
+		}
+  }
+}
