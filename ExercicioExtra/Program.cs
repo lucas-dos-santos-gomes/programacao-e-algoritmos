586d38ba5d58dfd17bd14cd10c504c146e79622d
Lucas dos Santos Gomes <lucas.sgomes26@senacsp.edu.br>
2023-05-01 20:27:22 -0300

    Finalizando exercício extra

--- ExercicioExtra/Program.cs
+++ ExercicioExtra/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+					
+public class ExercicioExtra
+{
+	public static void Main()
+	{
+		Console.WriteLine("Informe o valor da prestação do crédito imobiliário:");
+		double prestacao = double.Parse(Console.ReadLine());
+		Console.WriteLine("\nInforme seu salário bruto:");
+		double salario = double.Parse(Console.ReadLine());
+		double trintaPorCento = salario * 0.3;
+		
+		if(prestacao <= trintaPorCento) {
+			Console.WriteLine("\nCrédito aprovado!");
+		} else {
+			Console.WriteLine("\nCrédito não foi aprovado!");
+		}
+	}
+}
