1d2beed17a4ea292297fd37b0263c05faea5cbe0
Lucas dos Santos Gomes <lucas.sgomes26@senacsp.edu.br>
2023-05-01 20:17:17 -0300

    Finalizando exercício 7

--- Exercicio7/Program.cs
+++ Exercicio7/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+					
+public class Exercicio7
+{
+	public static void Main()
+	{
+		Console.WriteLine("Digite 1 (sinal vermelho) ou 2 (sinal verde):");
+		int sinal = int.Parse(Console.ReadLine());
+		while(sinal != 1 && sinal != 2) {
+			Console.WriteLine("\nIncorreto!");
+			Console.WriteLine("Digite 1 (sinal vermelho) ou 2 (sinal verde):");
+			sinal = int.Parse(Console.ReadLine());
+		}
+		if(sinal == 1) {
+			Console.WriteLine("\nESPERE!");
+		} else {
+			Console.WriteLine("\nSIGA!");
+		}
+	}
+}
