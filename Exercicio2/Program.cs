90754591fe751569c244274ad58d26b76dad5f2c
Lucas dos Santos Gomes <lucas.sgomes26@senacsp.edu.br>
2023-05-01 19:56:59 -0300

    Finalizando exercício 2

--- Exercicio2/Program.cs
+++ Exercicio2/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Exercicio2
+{
+  public static void Main()
+  {
+    Console.WriteLine("Digite um número inteiro: ");
+		int number = int.Parse(Console.ReadLine());
+		if(number % 2 == 0) {
+			Console.WriteLine("\nO número " + number + " é par!");
+		} else {
+			Console.WriteLine("\nO número " + number + " é impar!");
+		}
+  }
+}
