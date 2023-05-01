7a36bf27ac0760ee3dcf98407793a6859c3648d0
Lucas dos Santos Gomes <lucas.sgomes26@senacsp.edu.br>
2023-05-01 20:24:56 -0300

    Finalizando exercício 8

--- Exercicio8/Program.cs
+++ Exercicio8/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+					
+public class Exercicio8
+{
+	public static void Main()
+	{
+		Console.WriteLine("Digite o número de vitórias do Corinthians:");
+		int vitorias = int.Parse(Console.ReadLine());
+		Console.WriteLine("\nDigite o número de derrotas do Corinthians:");
+		int derrotas = int.Parse(Console.ReadLine());
+		
+		if(vitorias > derrotas) {
+			Console.WriteLine("\nBOM!");
+		} else if(vitorias < derrotas) {
+			Console.WriteLine("\nRUIM!");
+		} else {
+			Console.WriteLine("\nMAIS OU MENOS!");
+		}
+	}
+}
