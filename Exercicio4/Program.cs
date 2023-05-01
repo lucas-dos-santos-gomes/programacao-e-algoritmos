bc0443157ffa62a30941de32217e8d4a1a8d7012
Lucas dos Santos Gomes <lucas.sgomes26@senacsp.edu.br>
2023-05-01 20:09:31 -0300

    Finalizando exercício 4

--- Exercicio4/Program.cs
+++ Exercicio4/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Exercicio4
+{
+  public static void Main()
+  {
+    Console.WriteLine("Informe a receita da empresa:");
+		float receita = int.Parse(Console.ReadLine());
+		Console.WriteLine("\nInforme o prejuízo da empresa:");
+		float prejuizo = int.Parse(Console.ReadLine());
+		
+		if(receita > prejuizo) {
+			Console.WriteLine("\nA empresa teve LUCRO!");
+		} else if(receita < prejuizo) {
+			Console.WriteLine("\nA empresa teve PREJUIZO!");
+		} else {
+			Console.WriteLine("\nA empresa não teve LUCRO e nem PREJUÍZO!");
+		}
+  }
+}
