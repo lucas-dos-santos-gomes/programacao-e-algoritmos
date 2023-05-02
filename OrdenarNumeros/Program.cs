7c9c36549534010dc8743122846680d751ccc1a6
Lucas dos Santos Gomes <lucas.sgomes26@senacsp.edu.br>
2023-05-01 22:48:01 -0300

    Finalizando projeto 5 - OrdenarNumeros

--- OrdenarNumeros/Program.cs
+++ OrdenarNumeros/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+                   
+public class OrdenarNumeros
+{
+  public static void Main()
+  {
+    Console.WriteLine("Digite o primeiro valor: ");
+    int numA = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("\nDigite o segundo valor: ");
+    int numB = int.Parse(Console.ReadLine());
+    
+    Console.WriteLine("\nDigite o terceiro valor: ");
+    int numC = int.Parse(Console.ReadLine());
+    
+    if(numA > numB) {
+      int aux = numA;
+      numA = numB;
+      numB = aux;
+    }
+    if(numA > numC) {
+      int aux = numA;
+      numA = numC;
+      numC = aux;
+    }
+    if(numB > numC) {
+      int aux = numB;
+      numB = numC;
+      numC = aux;
+    }
+    Console.WriteLine("\nOrdem crescente: " + numA + " , " + numB + " , " + numC);
+  }
+}
