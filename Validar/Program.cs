84af3cc1517e59ca5a48ff2d69d619cef51d76e3
Lucas dos Santos Gomes <lucas.sgomes26@senacsp.edu.br>
2023-05-01 21:32:48 -0300

    Finalizando projeto 1 - Validar

--- Validar/Program.cs
+++ Validar/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Validar
+{
+  public static void Main()
+  {
+    Console.WriteLine("Digite um número de 0 a 100:");
+		int number = int.Parse(Console.ReadLine());
+		
+		if(number > 0 && number <= 100) {
+			Console.WriteLine("\nO número " + number + " é um valor VÁLIDO.");
+		} else {
+			Console.WriteLine("\nO número " + number + " é um valor INVÁLIDO.");
+		}
+  }
+}
