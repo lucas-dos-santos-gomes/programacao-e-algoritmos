f2e44193697aa63df17c2f7ecef1bf34717a8d51
Lucas dos Santos Gomes <lucas.sgomes26@senacsp.edu.br>
2023-05-02 10:41:30 -0300

    Finalizando exercício 2 - Classe Fibonacci

--- Fibonacci/Program.cs
+++ Fibonacci/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Fibonacci
+{
+  public static void Main()
+  {
+    string continuar;
+    do {
+      Console.WriteLine("\nDigite um número para fazer a sequência de Fibonacci:");
+      int index = int.Parse(Console.ReadLine());
+      int[] fibonacci = new int[index];
+      for(int i = 0; i < index; i++) {
+        if(i >= 2) {
+          fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+        } else {
+          fibonacci[i] = i;
+        }
+      }
+      string printFibonacci = "\nSequência de Fibonacci:";
+      foreach (int i in fibonacci) {
+        if(i == 0) {
+          printFibonacci += (" " + i);
+        } else {
+          printFibonacci += (", " + i);
+        }
+      }
+      Console.WriteLine(printFibonacci);
+      Console.WriteLine("\nDeseja fazer o Fibonacci novamente? (sim / nao)");
+      continuar = Console.ReadLine();
+      while(continuar != "sim" && continuar != "nao") {
+        Console.WriteLine("Digite apenas 'sim' ou 'nao': ");
+        continuar = Console.ReadLine();
+      }
+    } while(continuar == "sim");
+  }
+}
