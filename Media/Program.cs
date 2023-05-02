90e6df70fee70d870fd38e17e2495d60e47834b0
Lucas dos Santos Gomes <lucas.sgomes26@senacsp.edu.br>
2023-05-02 01:56:01 -0300

    Finalizando o exerício 1 - Classe Media

--- Media/Program.cs
+++ Media/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Media
+{
+  public static void Main()
+  {
+    int contador = 0;
+    double[] notas = new double[4];
+    foreach(double i in notas) {
+      contador++;
+      Console.WriteLine("\nInforme a " + contador + "ª nota");
+      notas[contador-1] = double.Parse(Console.ReadLine());
+    }
+    double media = (notas[0] + notas[1] + notas[2] + notas[3]) / 4;
+    Console.WriteLine("\nSua média foi " + media);
+  }
+}
