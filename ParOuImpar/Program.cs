ba4ddb1d3e370942807965eb7f3a0f79a4dde8f9
Lucas dos Santos Gomes <lucas.sgomes26@senacsp.edu.br>
2023-05-01 22:42:12 -0300

    Finalizando projeto 4 - ParOuImpar

--- ParOuImpar/Program.cs
+++ ParOuImpar/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		Console.WriteLine("Digite um número:");
+		int number = int.Parse(Console.ReadLine());
+		
+		if(number % 2 == 0) {
+			Console.WriteLine("\nO número " + number + " é par");
+		} else {
+			Console.WriteLine("\nO número " + number + " é ímpar");
+		}
+	}
+}
