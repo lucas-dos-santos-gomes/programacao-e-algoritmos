8357353cc75e944a39d4f7335999729c7a5fe8bb
Lucas dos Santos Gomes <lucas.sgomes26@senacsp.edu.br>
2023-05-02 00:08:24 -0300

    Finalizando projeto 6 - MeiaEntrada

--- MeiaEntrada/Program.cs
+++ MeiaEntrada/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class MeiaEntrada
+{
+  public static void Main()
+  {
+    Console.WriteLine("Digite seu nome:");
+		string name = Console.ReadLine();
+
+		Console.WriteLine("\nDigite sua idade:");
+		int age = int.Parse(Console.ReadLine());
+		
+    Console.WriteLine("\nVocê é estudante? (sim/nao)");
+		string yesOrNo = Console.ReadLine();
+		
+		while(yesOrNo != "sim" && yesOrNo != "nao") {
+			Console.WriteLine("\nVocê digitou um valor inválido!");
+			Console.WriteLine("Você é estudante? (sim/nao)");
+			yesOrNo = Console.ReadLine();
+		}
+		
+		bool student;
+		if(yesOrNo == "sim") {
+			student = true;
+		} else {
+			student = false;
+		}
+		
+		if(student || age < 12 || age > 65) {
+			Console.WriteLine("\nEntrada: meia");
+			Console.WriteLine("Valor: R$10,00");
+		} else {
+			Console.WriteLine("\nEntrada: inteira");
+			Console.WriteLine("Valor: R$20,00");
+		}
+  }
+}
