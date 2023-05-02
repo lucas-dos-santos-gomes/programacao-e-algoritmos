c242a3c9db71078e1f632cd0b59719fba1dddb7a
Lucas dos Santos Gomes <lucas.sgomes26@senacsp.edu.br>
2023-05-01 22:39:40 -0300

    Finalizando projeto 3 - Diferenca

--- Diferenca/Program.cs
+++ Diferenca/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+					
+public class Diferenca
+{
+	public static void Main()
+	{
+		Console.WriteLine("Digite o primeiro número:");
+		int number1 = int.Parse(Console.ReadLine());
+		
+		Console.WriteLine("\nDigite o segundo número");
+		int number2 = int.Parse(Console.ReadLine());
+		
+		if(number1 > number2) {
+			Console.WriteLine("\nMaior número: " + number1);
+			Console.WriteLine("Menor número: " + number2);
+			Console.WriteLine("Diferença: " + (number1 - number2));
+		} else {
+			Console.WriteLine("\nMaior número: " + number2);
+			Console.WriteLine("Menor número: " + number1);
+			Console.WriteLine("Diferença: " + (number2 - number1));
+		}
+	}
+}
