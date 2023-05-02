5646e80a9c3e40860d67b0d56619d0a5668bb194
Lucas dos Santos Gomes <lucas.sgomes26@senacsp.edu.br>
2023-05-02 00:11:29 -0300

    Finalizando projeto 7 - Ordenar

--- Ordenar/Program.cs
+++ Ordenar/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Ordenar
+{
+  public static void Main()
+  {
+    Console.WriteLine("Digite o primeiro valor:");
+		int numA = int.Parse(Console.ReadLine());
+		
+    Console.WriteLine("\nDigite o segundo número:");
+		int numB = int.Parse(Console.ReadLine());
+		
+    Console.WriteLine("\nDigite o terceiro valor:");
+		int numC = int.Parse(Console.ReadLine());
+		
+		if(numA <= numB && numB <= numC) {
+			Console.WriteLine("\nOrdem crescente: " + numA + " -> " + numB + " -> " + numC);
+		} else if(numA <= numC && numC <= numB) {
+			Console.WriteLine("\nOrdem crescente: " + numA + " -> " + numC + " -> " + numB);
+		} else if(numB <= numA && numA <= numC) {
+			Console.WriteLine("\nOrdem crescente: " + numB + " -> " + numA + " -> " + numC);
+		} else if(numB <= numC && numC <= numA) {
+			Console.WriteLine("\nOrdem crescente: " + numB + " -> " + numC + " -> " + numA);
+		} else if(numC <= numA && numA <= numB) {
+			Console.WriteLine("\nOrdem crescente: " + numC + " -> " + numA + " -> " + numB);
+		} else {
+			Console.WriteLine("\nOrdem crescente: " + numC + " -> " + numB + " -> " + numA);
+		}
+  }
+}
