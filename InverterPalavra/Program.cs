25b024fb414e4da90a53eb8cf2537321a77bf601
Lucas dos Santos Gomes <lucas.sgomes26@senacsp.edu.br>
2023-05-02 22:14:07 -0300

    Finalizando exercício 3 - Classe Fibonacci

--- InverterPalavra/Program.cs
+++ InverterPalavra/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class InverterPalavra
+{
+  public static void Main()
+  {
+    string continueProgram;
+    do {
+      string finalWord = "";
+      Console.WriteLine("\nEscreva uma palavra para invertermos: ");
+      string word = Console.ReadLine();
+      char[] wordList = word.ToCharArray();
+
+      for(int i = wordList.Length - 1; i >= 0; i--) {
+        finalWord += wordList[i];
+      }
+      Console.WriteLine("\nPalavra inserida: {0}", word);
+      Console.WriteLine("Palavra invertida: " + finalWord);
+
+      Console.WriteLine("\nDeseja inserir outra palavra? (sim / nao)");
+      continueProgram = Console.ReadLine();
+      while(continueProgram != "sim" && continueProgram != "nao") {
+        Console.WriteLine("\nInsira apenas 'sim' ou 'nao': ");
+        continueProgram = Console.ReadLine();
+      }
+    } while(continueProgram == "sim");
+  }
+}
