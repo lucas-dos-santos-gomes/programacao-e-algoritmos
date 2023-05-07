2c04633dc387eb50d312939e81576e0e5b46a5fb
Lucas dos Santos Gomes <lucas.sgomes26@senacsp.edu.br>
2023-05-07 03:14:00 -0300

    Finalizando exercício 4 - Palindromo

--- Palindrome/Palindrome.cs
+++ Palindrome/Palindrome.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Palindrome {
+  public static void Main() {
+    string continueProgram = "nao";
+    do {
+      Console.WriteLine("\nDigite uma palavra para verificarmos se é um palíndromo: ");
+      string word = Console.ReadLine();
+      string reverseWord = "";
+      for(int i = word.Length - 1; i >= 0; i--) {
+        reverseWord += word[i];
+      }
+
+      if(word == reverseWord) {
+        Console.WriteLine("\nEssa palavra é um palíndromo!");
+      } else {
+        Console.WriteLine("\nEssa palavra NÃO é um palíndromo!");
+      }
+
+      Console.WriteLine("\nDeseja inserir outra palavra? (sim / nao)");
+      continueProgram = Console.ReadLine();
+      while(continueProgram != "sim" && continueProgram != "nao") {
+        Console.WriteLine("\nInsira apenas 'sim' ou 'nao': ");
+        continueProgram = Console.ReadLine();
+      }
+    } while(continueProgram == "sim");
+  }
+}
