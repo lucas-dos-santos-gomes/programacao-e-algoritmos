9154c50ecce5f3f640a994c1012c224dc83a24a2
Lucas dos Santos Gomes <lucas.sgomes26@senacsp.edu.br>
2023-05-08 17:36:22 -0300

    Finalizando exercício 5 - Impares

--- Impares/Impares.cs
+++ Impares/Impares.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Impares
+{
+  public static void Main()
+  {
+    Random random = new Random();
+    int[] oddNumbers = new int[10];
+    string printConsole = "Primeiros 10 números ímpares embaralhados:";
+    for(int i = 0; i < oddNumbers.Length; i++) {
+      bool repeatedNumber;
+      int randomNumber;
+      if(i == 0) {
+        do {
+          randomNumber = random.Next(1, 22);
+        } while(randomNumber % 2 == 0);
+        printConsole += (" " + randomNumber);
+      } else {
+        do {
+          repeatedNumber = false;
+          do {
+            randomNumber = random.Next(1, 20);
+          } while(randomNumber % 2 == 0);
+          for(int j = 0; j < i; j++) {
+            if (randomNumber == oddNumbers[j]) {
+              repeatedNumber = true;
+              break;
+            }
+          }
+        } while(repeatedNumber);
+        printConsole += (", " + randomNumber);
+      }
+      oddNumbers[i] = randomNumber;
+    }
+    Console.WriteLine(printConsole);
+  }
+}
