4e75248c2d61cc8dd6d95ea639a020e95065921f
Lucas dos Santos Gomes <lucas.sgomes26@senacsp.edu.br>
2023-05-08 21:55:24 -0300

    Finalizando exercício 7 - TrocarPosicoes

--- TrocarPosicoes/TrocarPosicoes.cs
+++ TrocarPosicoes/TrocarPosicoes.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class TrocarPosicoes
+{
+  public static void Main()
+  {
+    Random random = new Random();
+    int[] firstArray = new int[16], finalArray = new int[16];
+    string printFirstArray = "\nArray principal:", printFinalArray = "Array inverso (8 / 8):";
+    for(int i = 0; i < firstArray.Length; i++) {
+      firstArray[i] = random.Next(1, 100);
+      if(i == 0) {
+        printFirstArray += (" " + firstArray[i]);
+      } else {
+        printFirstArray += (", " + firstArray[i]);
+      }
+    }
+    for(int i = 0; i < finalArray.Length; i++) {
+      if(i < 8) {
+        finalArray[i] = firstArray[i + 8];
+      } else {
+        finalArray[i] = firstArray[i - 8];
+      }
+      if(i == 0) {
+        printFinalArray += (" " + finalArray[i]);
+      } else {
+        printFinalArray += (", " + finalArray[i]);
+      }
+    }
+    Console.WriteLine(printFirstArray + "\n" + printFinalArray + "\n");
+  }
+}
