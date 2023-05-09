a6648249714a6b53dece9b8052c192977743e5d1
Lucas dos Santos Gomes <lucas.sgomes26@senacsp.edu.br>
2023-05-09 00:19:36 -0300

    Finalizando exercício 8 - ProcurarPrimeiraOcorrencia

--- ProcurarPrimeiraOcorrencia/ProcurarPrimeiraOcorrencia.cs
+++ ProcurarPrimeiraOcorrencia/ProcurarPrimeiraOcorrencia.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class ProcurarPrimeiraOcorrencia
+{
+  public static void Main()
+  {
+    Random random = new Random();
+    int[] numberList = new int[20];
+    for(int i = 0; i < numberList.Length; i++) {
+      numberList[i] = random.Next(1, 51);
+    }
+    Console.WriteLine("Digite um número que será buscado no Array: ");
+    
+    int searchNumber = int.Parse(Console.ReadLine());
+    int indexArray = Array.IndexOf(numberList, searchNumber);
+    if(indexArray >= 0) {
+     Console.WriteLine("\nO número " + searchNumber + " está na posição " + indexArray + " do array");
+    } else {
+      Console.WriteLine("\nO número " + searchNumber + " não está em nenhuma posição do array");
+    }
+    Console.WriteLine("\nItens do array:");
+    for(int i = 0; i < numberList.Length; i++) {
+      Console.WriteLine("{0}: {1}", i, numberList[i]);
+    }
+  }
+}
