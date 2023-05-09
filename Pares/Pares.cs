98e317e74617ae041a2ea46a845952a5fbb8f33b
Lucas dos Santos Gomes <lucas.sgomes26@senacsp.edu.br>
2023-05-09 02:17:53 -0300

    Finalizando exercício 10 - Pares

--- Pares/Pares.cs
+++ Pares/Pares.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Pares
+{
+  public static void Main()
+  {
+    Random random = new Random();
+    int[] numberArray = new int[40];
+    int evenCount = 0;
+    string printArray = "\nItens do array:";
+    for(int i = 0; i < numberArray.Length; i++) {
+      numberArray[i] = random.Next(1, 100);
+      if(numberArray[i] % 2 == 0) {
+        evenCount++;
+      }
+      string ternary = (i == 0) ? (" " + numberArray[i]) : (", " + numberArray[i]);
+      printArray += ternary;
+    }
+    Console.WriteLine(printArray);
+    Console.WriteLine("Quantidade de valores pares: {0}\n", evenCount);
+  }
+}
