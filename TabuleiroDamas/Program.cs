638afa101a7fc89c52e67e24f0eb894cdb0cc172
Lucas dos Santos Gomes <lucas.sgomes26@senacsp.edu.br>
2023-05-25 19:38:49 -0300

    Finalizando exercício TabuleiroDamas

--- TabuleiroDamas/Program.cs
+++ TabuleiroDamas/Program.cs
@@ -2,6 +2,49 @@
 
 public class TabuleiroDamas {
   public static void Main() {
-    Console.WriteLine("\nHello World!\n");
+    string columns = "";
+    for(int i = 0; i < 8; i++) {
+      columns += $"  {i} ";
+    }
+    Console.WriteLine(columns);
+    char[,] board = new char[8, 8];
+    for(int i = 0; i < 8; i++) {
+      string printBoard = "| ";
+      for(int j = 0; j < 8; j++) {
+        if(i < 3) {
+          if(i % 2 == 0) {
+            if(j % 2 == 0) {
+              board[i, j] = ' ';
+            } else {
+              board[i, j] = '0';
+            }
+          } else {
+            if(j % 2 == 0) {
+              board[i, j] = '0';
+            } else {
+              board[i, j] = ' ';
+            }
+          }
+        } else if(i > 4) {
+          if(i % 2 == 0) {
+            if(j % 2 == 0) {
+              board[i, j] = ' ';
+            } else {
+              board[i, j] = '*';
+            }
+          } else {
+            if(j % 2 == 0) {
+              board[i, j] = '*';
+            } else {
+              board[i, j] = ' ';
+            }
+          }
+        } else {
+          board[i, j] = ' ';
+        }
+        printBoard += board[i, j] + " | ";
+      }
+      Console.WriteLine(printBoard + i);
+    }
   }
 }
