2b921627d1200668ac9e28f9a9e7dc0b6fb0c257
Lucas dos Santos Gomes <lucas.sgomes26@senacsp.edu.br>
2023-05-29 23:44:43 -0300

    Finalizando exercício MaiorQueDez

--- MaiorQueDez/Program.cs
+++ MaiorQueDez/Program.cs
@@ -2,6 +2,25 @@
 
 public class MaiorQueDez {
   public static void Main() {
-    Console.WriteLine("Hello World!");
+    Random random = new Random();
+    int[,] matriz = new int[6, 6];
+    int lenght = matriz.GetLength(0);
+    int count = 0;
+    for(int i = 0; i < lenght; i++) {
+      string printMatriz = "|";
+      for(int j = 0; j < lenght; j++) {
+        matriz[i, j] = random.Next(1, 100);
+        if(matriz[i, j] < 10) {
+          printMatriz += ("0" + matriz[i, j] + "|");
+        } else {
+          printMatriz += (matriz[i, j] + "|");
+          if(matriz[i, j] > 10) {
+            count++;
+          }
+        }
+      }
+      Console.WriteLine(printMatriz);
+    }
+      Console.WriteLine("\nNúmeros maiores que dez: {0}", count);
   }
 }
