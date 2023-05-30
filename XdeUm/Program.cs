4cb9c9ad3f1b454e1d9cc7ac7350cb1a7bfea6e1
Lucas dos Santos Gomes <lucasdev.programador@gmail.com>
2023-05-30 00:00:22 -0300

    Criando estrutura de for

--- XdeUm/Program.cs
+++ XdeUm/Program.cs
@@ -3,5 +3,10 @@
 public class XdeUm {
   public static void Main(String[] args) {
     int[,] matriz = new int[5, 5];
+    for(int i = 0; i < matriz.GetLength(0); i++) {
+      for(int j = 0; j < matriz.GetLength(1); j++) {
+        
+      }
+    }
   }
 }
