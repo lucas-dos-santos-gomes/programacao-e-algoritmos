a58cc084702b050c60cd27c4dd1bcce3846061b0
Lucas dos Santos Gomes <lucas.sgomes26@senacsp.edu.br>
2023-05-29 21:35:47 -0300

    Finalizando projeto Diagonal

--- Diagonal/Program.cs
+++ Diagonal/Program.cs
@@ -2,6 +2,27 @@
 
 public class Diagonal {
   public static void Main() {
-    Console.WriteLine("Hello World!");
+    Random random = new Random();
+    int lenght = 8;
+    int[,] matriz = new int[lenght, lenght];
+    int diagonal1 = 0, diagonal2 = 0;
+    for(int i = 0; i < lenght; i++) {
+      string printMatriz = "|";
+      for(int j = 0; j < lenght; j++) {
+        matriz[i, j] = random.Next(0, 10);
+        if(i == j) {
+          diagonal1 += matriz[i, j];
+          printMatriz += "+ "+ matriz[i, j] + " +|";
+        } else if(i + j == 7) {
+          diagonal2 += matriz[i, j];
+          printMatriz += "- "+ matriz[i, j] + " -|";
+        } else {
+          printMatriz += "  "+ matriz[i, j] + "  |";
+        }
+      }
+      Console.WriteLine(printMatriz);
+    }
+    Console.WriteLine("\nPrimeira diagonal (+): {0}", diagonal1);
+    Console.WriteLine("Segunda diagonal (-): {0}", diagonal2);
   }
 }
