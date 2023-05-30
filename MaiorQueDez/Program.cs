3b04d172db797de16093bd51333e076995662677
Lucas dos Santos Gomes <lucas.sgomes26@senacsp.edu.br>
2023-05-29 21:49:58 -0300

    Fazendo Hello World no MaiorQueDez

--- MaiorQueDez/Program.cs
+++ MaiorQueDez/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class MaiorQueDez {
+  public static void Main() {
+    Console.WriteLine("Hello World!");
+  }
+}
