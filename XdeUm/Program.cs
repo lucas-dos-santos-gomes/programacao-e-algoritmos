f4655b1c4e616c686796d0a430b06043a9b3c1e4
Lucas dos Santos Gomes <lucasdev.programador@gmail.com>
2023-05-30 09:22:58 -0300

    Finalizando exercício XdeUm

--- XdeUm/Program.cs
+++ XdeUm/Program.cs
@@ -3,10 +3,19 @@
 public class XdeUm {
   public static void Main(String[] args) {
     int[,] matriz = new int[5, 5];
+    Console.WriteLine("\n");
     for(int i = 0; i < matriz.GetLength(0); i++) {
+      string printMatriz = "| ";
       for(int j = 0; j < matriz.GetLength(1); j++) {
-        
+        if(i == j || i + j == (matriz.GetLength(0) - 1)) {
+          matriz[i, j] = 1;
+        } else {
+          matriz[i, j] = 0;
+        }
+        printMatriz += matriz[i, j] + " | ";
       }
+      Console.WriteLine(printMatriz);
     }
+    Console.WriteLine("\n");
   }
 }
