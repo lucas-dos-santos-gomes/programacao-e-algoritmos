c88f343b54dc151721a6d63d3fc4a14e00ef72f7
Lucas dos Santos Gomes <lucasdev.programador@gmail.com>
2023-06-02 01:34:41 -0300

    Tentando usar mais uma classe na pirâmide

--- Piramide/Program.cs
+++ Piramide/Program.cs
@@ -1,11 +1,39 @@
 ﻿using System;
 
 public class Piramide {
+  public static string PrintSpaces(char[,] matriz, int line, int columns, int spaces, int stars) {
+    string printMatriz = "";
+    for(int j = 0; j < columns; j++) {
+      for (int k = 0; k < spaces; k++) {
+        matriz[line, j] = ' ';
+        printMatriz += matriz[line, j];
+      }
+      for (int k = 0; k < stars; k++) {
+        matriz[line, j] = '*';
+        printMatriz += matriz[line, j];
+      }
+      for (int k = 0; k < spaces; k++) {
+        matriz[line, j] = ' ';
+        printMatriz += matriz[line, j];
+      }
+      
+      if(spaces > 0) {
+        spaces--;
+        stars += 2;
+      }
+      printMatriz += "\n";
+    }
+    return printMatriz += "\n";
+  }
   public static void Main(String[] args) {
     int lines = 6;
     int columns = 11;
     int spaces = columns / 2;
+    int stars = 1;
     char[,] pyramid = new char[lines, columns];
-    Console.WriteLine(spaces);
+    for(int i = 0; i < lines; i++) {
+      Console.WriteLine(PrintSpaces(pyramid, i, columns, spaces, stars));
+      Console.WriteLine(pyramid[0, 0]);
+    }
   }
 }
