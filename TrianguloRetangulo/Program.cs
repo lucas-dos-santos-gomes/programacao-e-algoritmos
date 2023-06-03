e99008d186260d634c0f5ea64d6bfc7c04dc1aad
Lucas dos Santos Gomes <lucasdev.programador@gmail.com>
2023-06-02 21:43:10 -0300

    Finalizando Projeto TrianguloRetangulo

--- TrianguloRetangulo/Program.cs
+++ TrianguloRetangulo/Program.cs
@@ -2,6 +2,25 @@
 
 public class TrianguloRetangulo {
   public static void Main() {
-    Console.WriteLine("Hello World!");
+    int length = 6, stars = 1, countStars = 0;
+    char[,] triangle = new char[length, length];
+    for (int i = 0; i < length; i++) {
+      Console.Write("|");
+      for (int j = 0; j < length; j++) {
+        if(j == 0) {
+          countStars = stars;
+        }
+
+        if(countStars > 0) {
+          triangle[i, j] = '*';
+          countStars--;
+        } else {
+          triangle[i, j] = ' ';
+        }
+        Console.Write(triangle[i, j] + "|");
+      }
+      stars++;
+      Console.Write("\n");
+    }
   }
 }
