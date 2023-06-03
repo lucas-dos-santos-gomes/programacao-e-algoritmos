2e40895ea4c087e4767594d2fe7a690f4fb95bf1
Lucas dos Santos Gomes <lucasdev.programador@gmail.com>
2023-06-02 22:17:39 -0300

    Iniciando o gabarito do projeto

--- Gabarito/Program.cs
+++ Gabarito/Program.cs
@@ -1,7 +1,27 @@
 ﻿using System;
 
 public class Gabarito {
+  private static char Letra(int numero) {
+    switch(numero) {
+      case 1:
+        return 'a';
+      case 2:
+        return 'b';
+      case 3:
+        return 'c';
+      case 4:
+        return 'd';
+      default:
+        return 'e';
+    }
+  }
   public static void Main(String[] args) {
-    Console.WriteLine("Hello World!");
+    Random random = new Random();
+    char[] gabarito = new char[10];
+    for(int i = 0; i < gabarito.Length; i++) {
+      int numero = random.Next(1, 6);
+      gabarito[i] = Letra(numero);
+      Console.Write(gabarito[i] + " | ");
+    }
   }
 }
