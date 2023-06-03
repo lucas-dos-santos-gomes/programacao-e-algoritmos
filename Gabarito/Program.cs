b20c048a7293b6d726e21d7272f505eab90722f4
Lucas dos Santos Gomes <lucasdev.programador@gmail.com>
2023-06-02 22:50:42 -0300

    Completando variáveis do Gabarito

--- Gabarito/Program.cs
+++ Gabarito/Program.cs
@@ -18,10 +18,22 @@ public class Gabarito {
   public static void Main(String[] args) {
     Random random = new Random();
     char[] gabarito = new char[10];
+    Console.Write("\nGabarito: |");
     for(int i = 0; i < gabarito.Length; i++) {
       int numero = random.Next(1, 6);
       gabarito[i] = Letra(numero);
-      Console.Write(gabarito[i] + " | ");
+      Console.Write(gabarito[i] + "|");
+    }
+    Console.WriteLine("\n\n- Respostas -");
+    int[,] respostasAlunos = new int[100, gabarito.Length];
+    int[] resultados = new int[100];
+    for(int i = 0; i < respostasAlunos.GetLength(0); i++) {
+      int resultado = 0;
+      for(int j = 0; j < gabarito.Length; j++) {
+        respostasAlunos[i, j] = random.Next(1, 6);
+        resultado += (Letra(respostasAlunos[i, j]) == gabarito[j])? 1 : 0;
+      }
+      resultados[i] = resultado;
     }
   }
 }
